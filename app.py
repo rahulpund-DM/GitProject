6beafa2543d743e8e46f526d8dd4b556933ecd4f
rahulpund-DM <rahul.pund@datamites.in>
2026-06-13 13:24:09 +0530

    multiplication

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 def add(a,b,c):
     return a+b+c
 
-<<<<<<< HEAD
 def sub(a,b,d):
     return a-b-d
-=======
-def sub(a,b,e):
-    return a-b-e
->>>>>>> 17601512433370bfad521633396ad4d150421ca4
 
 def div(a,b):
     return a/b
+
+def mul(a,b):
+    return a*b
